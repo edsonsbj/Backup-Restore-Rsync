8d042b66a260d527a540b949036877a7140a7866
edsonsbj <60072822+edsonsbj@users.noreply.github.com>
2022-04-28 11:48:37 -0300

    Add files via upload

--- Backup.sh
+++ Backup.sh
@@ -3,62 +3,52 @@
 
 # Script Simples para a realização de backup e restauração de pastas e arquivos usando Rsync em HD Externo 
  
-CONFIG="/path/to/Rsync/Configs"
+# Adicione aqui o caminho para o Arquivo Configs
+CONFIG="/Path/to/Nextcloud-Backup-Restore/Configs" 
+
 . ${CONFIG}
 
-# NOT CHANGE
+# NÃO ALTERE
 MOUNT_FILE="/proc/mounts"
 NULL_DEVICE="1> /dev/null 2>&1"
 REDIRECT_LOG_FILE="1>> $LOGFILE_PATH 2>&1"
 # ------------------------------------------------------------------------ #
 # -------------------------------TESTS----------------------------------------- #
-# Is root?
+# Script Executado como root?
 [ "$UID" != "0" ] && {
   echo "---------- You must be root ----------" >> $LOGFILE_PATH
   exit 1
 }
 
-# Is device mounted?
+# O Dispositivo está Montado?
 grep -q "$DEVICE" "$MOUNT_FILE"
 if [ "$?" != "0" ]; then
-  # If not, mount to $DESTINATIONDIR
-  echo "---------- Device not mounted. Mounting $DEVICE ----------" >> $LOGFILE_PATH
+  # Se não, monte em $DESTINATIONDIR
+  echo "---------- Dispositivo não montado. Monte $DEVICE ----------" >> $LOGFILE_PATH
   eval mount -t auto "$DEVICE" "$DESTINATIONDIR" "$NULL_DEVICE"
 else
-  # If yes, grep the mount point and change the $DESTINATIONDIR
+  # Se sim, grep o ponto de montagem e altere o $DESTINATIONDIR
   DESTINATIONDIR=$(grep "$DEVICE" "$MOUNT_FILE" | cut -d " " -f 2)
 fi
 
 cd "/"
 
-# Is there write permissions?
+# Há permissões de excrita e gravação?
 [ ! -w "$DESTINATIONDIR" ] && {
-  echo "---------- Do not have write permissions ----------" >> $LOGFILE_PATH
+  echo "---------- Não tem permissões de gravação ----------" >> $LOGFILE_PATH
   exit 1
 }
 ## ------------------------------------------------------------------------ #
 
-  echo "---------- Back-up initiated. ----------" >> $LOGFILE_PATH
+  echo "---------- Iniciando Backup. ----------" >> $LOGFILE_PATH
 
 # -------------------------------FUNCTIONS----------------------------------------- #
-# Exemplo para realizar backup e restauração de vários diretórios. Comente a linha abaixo se naão tiver interesse
-
-backup() {
-sudo rsync -avh --delete --progress {"$DIR01","$DIR02"} "$DESTINATIONDIR" 1>> $LOGFILE_PATH
-
-# Exemplo para realizar backup e restauração de um único diretório e filtrar os arquivos que sera realizado backup. Comente a linha abaixo se não tiver interesse
-
 backup() {
 sudo rsync -avh --delete --progress "$DIR01" "$DESTINATIONDIR" --files-from "$INCLIST" 1>> $LOGFILE_PATH
 
-# Exemplo para realizar backup simples de um único diretório. Comente a linha abaixo se não tiver interesse
-
-backup() {
-sudo rsync -avh --delete --progress "$DIR01" "$DESTINATIONDIR" 1>> $LOGFILE_PATH
-
-  # Worked fine? Umount.
+  # Funcionou bem? Remova a Midia Externa.
   [ "$?" = "0" ] && {
-    echo "---------- Back-up finished. Umounting $DEVICE ----------" >> $LOGFILE_PATH
+    echo "---------- Backup Finalizado. Desmonte a Unidade $DEVICE ----------" >> $LOGFILE_PATH
  	eval umount "$DEVICE" "$NULL_DEVICE"
 	eval sudo udisksctl power-off -b "${DEVICE}" >>$LOGFILE_PATH
     exit 0
@@ -66,7 +56,7 @@ sudo rsync -avh --delete --progress "$DIR01" "$DESTINATIONDIR" 1>> $LOGFILE_PATH
 }
 
 preparelogfile () {
-  # Insert a simple header to the log file with the timestamp
+  # Insira um cabeçalho simples no arquivo de log com o carimbo de data/hora
   echo "----------[ $(date) ]----------" >> $LOGFILE_PATH
 }
 
